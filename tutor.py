612f6f1f9515ac9702288d06bf306981df6d3787
yunPKU <46952732+yunPKU@users.noreply.github.com>
2020-01-05 14:08:26 +0800

    Update tutor.py

--- tutor.py
+++ tutor.py
@@ -6,22 +6,22 @@ Created on Sun Jan  5 11:48:51 2020
 @author: macbjmu
 """
 
-import HeteroInfer as HI
+import simuEst as SE
 
 
 """ 
    Single Simulation and analysis
 """ 
-simuG = HI.singleSimu(mu_InFC = 1,mu_Rec = 1/2.5);
-inferRe 	= HI.singleEst(simuG, back_ratio = [0.9,0.85,0.8], smpRatio = 0.9)
+simuG = SE.singleSimu(mu_InFC = 1,mu_Rec = 1/2.5);
+inferRe 	= SE.singleEst(simuG, back_ratio = [0.9,0.85,0.8], smpRatio = 0.9)
 
 
 """ multiple simulaiton and analysis
 """
-res = HI.cmpEst(mu_InFC = 1,mu_Rec = 1/2.5,simu_K = 2)
+res = SE.cmpEst(mu_InFC = 1,mu_Rec = 1/2.5,simu_K = 2)
 HI.EstRePlot(re)
 
 """ Analyze tree files 
 """
 
-res = HI.anaTree(filename = filenm)
+res = SE.anaTree(filename = filenm)
